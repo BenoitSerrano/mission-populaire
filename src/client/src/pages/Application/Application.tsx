0e8788d07cca2cc6c3bee0080daea345eb8f2654
Benoit Serrano <benoit.serrano10@gmail.com>
2025-02-22 13:21:49 +0100

    design application

--- src/client/src/pages/Application/Application.tsx
+++ src/client/src/pages/Application/Application.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { applicationsApi } from '../../lib/api/applicationsApi';
 import { Query } from '../../components/Query';
+import { styled } from '@mui/material';
 
 function Application() {
     const params = useParams<{ applicationId: string }>();
@@ -8,9 +9,28 @@ function Application() {
     const getApplication = () => applicationsApi.getApplication({ applicationId });
     return (
         <Query apiCall={getApplication} queryKey={['applications', applicationId]}>
-            {(application) => <div />}
+            {(application) => (
+                <Container>
+                    <Page>truc</Page>
+                </Container>
+            )}
         </Query>
     );
 }
+
+const Container = styled('div')(({ theme }) => ({
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+const Page = styled('div')(({ theme }) => ({
+    background: theme.palette.common.white,
+    color: theme.palette.common.black,
+    border: `solid 1px ${theme.palette.common.black}`,
+    borderRadius: '2px',
+    width: '40%',
+    height: '95%',
+}));
 
 export { Application };
